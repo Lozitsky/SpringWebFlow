e5e7a2e2cb8647ffc029236f7dd9512d3143ce26
Kirilo Lozitsky <bondxxl01@gmail.com>
2019-07-19 02:32:00 +0300

    changed logging on log4j2. Added flow local pattern for automatic flows registration. Fixed configuration in pom.xml.

--- src/main/java/com/kirilo/springMVC/models/User.java
+++ src/main/java/com/kirilo/springMVC/models/User.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 //    https://intellij-support.jetbrains.com/hc/en-us/community/posts/206957655-What-happened-to-the-generate-serialVersionUID-feature-?page=1#community_comment_206678425
 //    https://stackoverflow.com/questions/888335/why-generate-long-serialversionuid-instead-of-a-simple-1l
 public class User implements Serializable {
-    private static final long serialVersionUID = 16456546546546L;
+    private static final long serialVersionUID = 1L;
 
     private String name;
     private String password;
